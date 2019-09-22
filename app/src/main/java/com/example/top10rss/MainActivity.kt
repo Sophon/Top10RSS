1a370b25bcaa0a4f8ec5e857bed8720245807741
Le Duy Anh <lenduya@gmail.com>
2019-09-22 20:03:35 +0200

    Implement DownloadData

--- app/src/main/java/com/example/top10rss/MainActivity.kt
+++ app/src/main/java/com/example/top10rss/MainActivity.kt
@@ -3,21 +3,39 @@ package com.example.top10rss
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG, "onCreate: done")
     }
 
-    private inner class DownloadData: AsyncTask<String, Void, String>() {
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-        }
+    companion object {
+        private class DownloadData: AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
 
-        override fun doInBackground(vararg params: String?): String {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute: par is $result")
+            }
+
+            override fun doInBackground(vararg urls: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${urls[0]}")
+                val rssFeed = downloadXML(urls[0])
+                if(rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: error downloading")
+                }
+
+                return rssFeed
+            }
         }
     }
 }
