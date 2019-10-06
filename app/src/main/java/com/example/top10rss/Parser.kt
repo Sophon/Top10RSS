3a8acdb4ada3da833f7bab5d18827d3f2ca5cb5f
lenduya <lenduya@gmail.com>
2019-10-06 20:50:14 +0200

    Start implementing Parser class

---
+++ app/src/main/java/com/example/top10rss/Parser.kt
@@ -0,0 +1,37 @@
+package com.example.top10rss
+
+import android.util.Log
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.lang.Exception
+
+class Parser {
+    private val TAG = "Parser"
+
+    val applications = ArrayList<FeedEntry>()
+
+    fun parse(xmlData: String): Boolean {
+        Log.d(TAG, "parse called with $xmlData")
+        var status = true
+        var isEntry = false
+        var textValue = ""
+
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            factory.isNamespaceAware = true
+            val xpp = factory.newPullParser()
+            xpp.setInput(xmlData.reader())
+            var eventType = xpp.eventType
+            var currentRecord = FeedEntry()
+
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+
+            }
+        } catch(e: Exception) {
+            e.printStackTrace()
+            status = false
+        }
+
+        return status
+    }
+}
