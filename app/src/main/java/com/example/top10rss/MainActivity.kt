e6b2311897977af9dfb38b733462245c415cee10
lenduya <lenduya@gmail.com>
2019-10-11 18:14:26 +0200

    data -> arrayadapter

--- app/src/main/java/com/example/top10rss/MainActivity.kt
+++ app/src/main/java/com/example/top10rss/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.top10rss
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -14,20 +19,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate: called")
-        val downloadData = DownloadData()
+        val downloadData = DownloadData(this, xmlListView)
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
     }
 
     companion object {
-        private class DownloadData: AsyncTask<String, Void, String>() {
+        private class DownloadData(context: Context, listView: ListView): AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
+
+            var propContext by Delegates.notNull<Context>()
+            var propListView by Delegates.notNull<ListView>()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
 
             override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-//                Log.d(TAG, "onPostExecute: par is $result")
                 val parser = Parser()
                 parser.parse(result)
+
+                val arrayAdapter = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parser.applications)
+                propListView.adapter = arrayAdapter
             }
 
             override fun doInBackground(vararg urls: String?): String {
